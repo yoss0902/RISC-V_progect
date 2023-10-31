775f5217a6b3fa8b47de8f7fc037ef0ed00ba091
yoss <yosi.ida@mail.huji.ac.il>
2023-10-31 19:02:04 +0200

    dff

---
+++ Day_2/day2.sv
@@ -0,0 +1,32 @@
+// Day 2
+// various kinds of D flip-flop
+
+module D_FF(q_non_rst, q_sncr_rst, q_asncr_rst, d_in, clk, rst);
+
+	input d_in;
+	input clk;
+	input rst;
+	output reg q_non_rst;
+	output reg q_sncr_rst;
+	output reg q_asncr_rst;
+
+	// no reset
+	always@(posedge clk)
+		q_non_rst <= d_in;
+
+	//synchronize reset
+	always@(posedge clk)
+		if (rst)
+			q_sncr_rst <= 1'b0;
+		else
+			q_sncr_rst <= d_in;
+	
+	//asynchronize reset
+	always@(posedge clk or posedge rst)
+		if (rst)
+			q_asncr_rst <= 1'b0;
+		else
+			q_asncr_rst <= d_in;
+	
+endmodule
+				
---
+++ Day_2/day2_tb.sv
@@ -0,0 +1,38 @@
+module D_FF_tb;
+	
+	reg clk;
+	reg d_in;
+	reg rst;
+	wire q_non_rst;
+	wire q_sncr_rst;
+	wire q_asncr_rst;
+
+	D_FF d_ff_dut(.clk(clk), .d_in(d_in), .rst(rst), .q_non_rst(q_non_rst), .q_sncr_rst(q_sncr_rst), .q_asncr_rst(q_asncr_rst);
+
+	initial
+		$monitor("time = %d, clk = %b, d_in =%b, rst = %b, q_non_rst = %b, q_sncr_rst = %b, q_asncr_rst = %b", $time, clk, d_in, rst, q_non_rst, q_sncr_rst, q_asncr_rst);
+
+	initial begin
+		clk = 1'b0;
+		rst = 1'b0;
+		d_in = 1'b0;
+	end
+
+	always #5 clk = ~clk;
+
+	initial begin
+		#10 d_in = 1'b1;
+		#10 rst = 1'b1;
+		#10 rst = 1'b0;
+		#10 d_in = 1'b0;
+		#5 rst = 1'b1;
+	       	d_in = 1'b1;
+		#10 rst = 1'b0;
+		#20 $finish;
+	end
+endmodule
+
+
+
+			
+	

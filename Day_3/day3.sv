e6e548da3225cd6554efc08262ec5f24a9e84169
yoss <yosi.ida@mail.huji.ac.il>
2023-11-01 16:51:17 +0200

    edge_detector

---
+++ Day_3/day3.sv
@@ -0,0 +1,25 @@
+module edge_dect(fall_edge, rise_edge, clk, rst, d_in);
+	
+	input clk;
+	input rst;
+	input d_in;
+
+	output fall_edge;
+	output rise_edge;
+
+	reg q;
+
+	always@(posedge clk, posedge rst)
+		if (rst)
+			q <= 1'b0;
+		else
+			q <= d_in;
+
+	// positive edge detector
+	assign rise_edge = ~q & d_in;
+
+	//negative edge detector
+	assign neg_edge = q & ~d_in;
+
+endmodule
+
---
+++ Day_3/day3_tb.sv
@@ -0,0 +1,38 @@
+module edge_dect_tb;
+	
+	reg clk;
+	reg rst;
+	reg d_in;
+	reg q;
+	
+	wire rise_edge;
+	wire fall_edge;
+	
+	edge_dect edge_dect_dut(.clk(clk), .rst(rst), .d_in(d_in), .q(q), .rise_edge(rise_edge), .fall_edge(fall_edge));
+
+	initial begin
+		$dumpfile("Day_3.vcd");
+		$dumpvars;
+
+	initial
+		$monitor("time =%d, clk = %b, rst = %b, d_in = %b, q = %b, rise_edge = %b, fall_edge = %b", $time, clk, rst, d_in, q, rise_edge, fall_edge);
+
+	initial begin
+		clk = 1'b0;
+		rst = 1'b0;
+		d_in = 1'b0;
+	end
+
+	always #5 clk = ~clk;
+
+	initial begin
+		#10 d_in = 1'b1;
+		#10 rst = 1'b1;
+		#10 rst = 1'b0;
+		#10 d_in = 1'b0;
+		#10 d_in = 1'b1;
+		#20 $finish;
+	end
+
+endmodule
+			

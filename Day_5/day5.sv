7cde0947f3d09103835631c539ed86e2d9e7b30c
yoss <yosi.ida@mail.huji.ac.il>
2023-11-05 19:38:47 +0200

    odd counter

---
+++ Day_5/day5.sv
@@ -0,0 +1,16 @@
+//8 bit odd counter
+
+module odd_cntr(cntr_out, clk, rst);
+	
+	input clk;
+	input rst;
+
+	output reg [7:0]cntr_out
+
+	always@(posedge clk or posedge rst)
+		if (rst | cntr_out == 8'hff)
+			cntr_out <= 8'd1;
+		else
+			cntr_out <= cntr_out + 8'd2;
+	
+endmodule
---
+++ Day_5/day5_tb.sv
@@ -0,0 +1,28 @@
+module odd_cntr_tb;
+	
+	reg clk;
+	reg rst;
+
+	wire [7:0]cntr_out;
+
+	odd_cntr odd_cntr_dut(.clk(clk), .rst(rst), .cntr_out(cntr_out))
+
+	initial begin
+		$dumpfile("day5.vcd");
+		$dumpvars;
+
+	end
+
+	initial begin
+		clk = 1'b0;
+		rst = 1'b0;
+	end
+
+	always #5 clk = ~clk
+
+	initial begin
+		#30 rst = 1'b1;
+		#10 rst = 1'b0;
+		#1220 
+endmodule
+

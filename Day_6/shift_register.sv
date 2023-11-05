2b22b77e53b029dc7fa735f6ddb1d8b10c106abd
yoss <yosi.ida@mail.huji.ac.il>
2023-11-05 20:28:47 +0200

    shift register

---
+++ Day_6/shift_register.sv
@@ -0,0 +1,16 @@
+module shift_register(sft_out, in, clk, rst);
+	
+	input in;
+	input clk;
+	input rst;
+
+	output [3:0]sft_out;
+	
+	always@(posedge clk or posedge rst)
+		if (rst) 
+			sft_out <= 4'b0
+		else
+			sft_out <= {in, sft_out[3:1];
+	end
+
+endmodule
---
+++ Day_6/shift_register_tb.sv
@@ -0,0 +1,34 @@
+module shift_register_tb;
+	
+	reg clk;
+	reg rst;
+	reg in;
+
+	wire [3:0] sft_out;
+
+	shift_register sft_reg_dut(.clk(clk), .rst(rst), .in(in), .sft_out(sft_out));
+
+	initial begin
+		$dumpfile("day6.vcd");
+		$dumpvars;
+
+	end
+	
+	initial begin
+		clk = 1'b0;
+		rst = 1'b0;
+		in = 1'b0;
+	end
+
+	always #5 clk = ~clk;
+	
+	initial begin
+		#10 rst = 1'b1;
+		#10 rst = 1'b0;
+		repeat(20)
+			in = $random%2;
+			#10;
+		$finish;
+	end
+
+endmosule

ad6a7e0636a423b242aac32b271b0d4b7a6f4c87
yoss <yosi.ida@mail.huji.ac.il>
2023-11-08 19:26:19 +0200

    binary to gray convertor

--- Day_9/bin_to_gray_conv.sv
+++ Day_9/bin_to_gray_conv.sv
@@ -9,7 +9,7 @@ module bin_to_gray_conv(bin_in, gray_out);
 	assign gray_out[bit_num - 1] = bin_in[bit_num - 1];
        	
 	genvar i;	
-	for (i = bit_num - 2, i>=0, i = i-1)
+	for (i = bit_num - 2; i>=0; i = i-1)
 		assign gray_out[i] = bin_in[i+1]^bin_in[i];
 	
 

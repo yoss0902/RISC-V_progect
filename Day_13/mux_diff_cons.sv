419495b843362e5f0964ff82f908d4f5cf59aa83
yoss <yosi.ida@mail.huji.ac.il>
2023-11-20 18:14:18 +0200

    diffrent construction of 4:1 mux

--- Day_13/mux_diff_cons.sv
+++ Day_13/mux_diff_cons.sv
@@ -6,31 +6,32 @@ module mux_diff_cons(ter_out, case_out, ifelse_out, aor_out, sel, in);
 	output ter_out;
 	output case_out;
 	output ifelse_out;
-	output aor out;
+	output aor_out;
 	
-	assign ter_out = sel[0] ? (s[1] ? ter_out = sel[3] : ter_out = sel[1]) :
-	       			  (s[1] ? ter_out = sel[2] : sel_out = sel[0])
+	assign ter_out = sel[0] ? (sel[1] ? in[3] : in[1]) :
+	       			  (sel[1] ? in[2] : in[0]) ;
 
-	always@(posedge sel or negedge sel)
-		case(sel) begin
-			3'h0 : case_out = sel[0];
-			3'h1 : case_out = sel[1];
-			3'h2 : case_out = sel[2];
-			3'h3 : case_out = sel[3];
+	always@(*) begin
+		case(sel) 
+			3'h0 : case_out = in[0];
+			3'h1 : case_out = in[1];
+			3'h2 : case_out = in[2];
+			3'h3 : case_out = in[3];
 		endcase
+	end
 
-	always@(posedge sel or negedge sel)
+	always@(*)
 		if (sel[0] & sel[1])
-			ifelse_out = sel[3];
+			ifelse_out = in[3];
 		else if (sel[0] &~sel[1])
-			ifelse_out = sel[1]	
+			ifelse_out = in[1];	
 		else if (~sel[0] & sel[1])
-			ifelse_out = sel[2];
-		else if (~sel[o] & ~sel[1])
-			ifelse-out = sel[0];
+			ifelse_out = in[2];
+		else if (~sel[0] & ~sel[1])
+			ifelse_out = in[0];
 
 
-	assign aor_out = ((((in[1]&s[0])|(in[0]&(~sel[0])))&((~sel[1]))) | 
-		         (((in[3]&s[0])|(in[2]&(~sel[0])))&sel[1]));
+	assign aor_out = ((((in[1]&sel[0])|(in[0]&(~sel[0])))&((~sel[1]))) | 
+		         (((in[3]&sel[0])|(in[2]&(~sel[0])))&sel[1]));
 
 endmodule

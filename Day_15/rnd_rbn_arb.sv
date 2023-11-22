4d378c45cea518be7f5e271206205c64eb0f2996
yoss <yosi.ida@mail.huji.ac.il>
2023-11-22 13:28:54 +0200

    4-bit round robin arbiter

--- Day_15/rnd_rbn_arb.sv
+++ Day_15/rnd_rbn_arb.sv
@@ -5,19 +5,29 @@ module rnd_rbn_arb(gnt_out, clk, rst, req_in);
 	input [3:0]req_in;
 
 	output [3:0]gnt_out;
-	wire [1:0]counter;
+	wire [2:0]counter;
 
 	always@(posedge clk or posedge rst)
-		if (rst or counter == 2'h4)
-			counter <= 1'h0;
-		else begin
-			while (~req_in[counter]) begin
-				counter <= counter + 1'h1;
-				if (counter == 2'h5)
-					counter <= 1'h0;
-			end
-			gnt_out <= req_in[counter]; 
+		if (rst | counter == 2'h4) begin
+			gnt_out = 4'h0;
+			counter = 3'h0;
 		end
+		else begin
+			counter = counter + 1
+		end
+
+	always@(*)
+		if (req_in[counter]) begin
+			gnt_out = 4'h0;
+			gnt_out[counter] = 1'h1;
+		end
+		else if (counter == 2'h4)
+			counter = 3'h0;
+		else
+			counter = counter +1;
+
+
+
 	
 endmodule
 
